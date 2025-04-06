883f24c205504be7e8a35e9678e6d7bdd5a7a0e9
ehsanmehrali428@gmail.com <ehsanmehrali428@gmail.com>
2025-04-06 12:26:50 +0200

    Implementing the loop for right and wrong letters

--- snowman.py
+++ snowman.py
@@ -33,6 +33,7 @@ STAGES = [
      """
  ]
 
+
 def get_random_word():
     """Selects a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
@@ -48,23 +49,38 @@ def display_game_state(mistakes, secret_word, guessed_letters):
             display_word += letter + " "
         else:
             display_word += "_ "
+    mistakes += 1
     print("Word: ", display_word)
     print("\n")
+    return mistakes
+
 
 
 def play_game():
     secret_word = get_random_word()
-    print(secret_word)
     guessed_letters = []
     mistakes = 0
-
+    max_mistake = len(STAGES)
+    print(secret_word)
     print("Welcome to Snowman Meltdown!")
     # For now, display the initial game state.
-    display_game_state(mistakes, secret_word, guessed_letters)
+    while mistakes < max_mistake:
+        display_game_state(mistakes, secret_word, guessed_letters)
 
-    # Prompt user for one guess (logic to be enhanced later)
-    guess = input("Guess a letter: ").lower()
-    print("You guessed:", guess)
+        # Prompt user for one guess (logic to be enhanced later)
+        guess = input("Guess a letter: ").lower()
+        guessed_letters.append(guess)
+        # print("You guessed:", guess)
+
+        if guess not in secret_word:
+            mistakes += 1
+            print(f"OOPS! '{guess}' is not in the word.\n")
+
+        answer = [letter in guessed_letters for letter in secret_word]
+        if all(answer):
+            display_game_state(mistakes, secret_word, guessed_letters)
+            print("🎉 Congratulations! You saved the snowman!")
+            return
 
 
 if __name__ == "__main__":
